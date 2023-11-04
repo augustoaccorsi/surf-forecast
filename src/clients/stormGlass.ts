1f2a7779923c0acaefaf3a3574f08bbfdfe67f2c
augustoaccorsi <augusto.acorsi@gmail.com>
2023-11-03 23:13:24 -0300

    add stormglass client class

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosStatic } from 'axios';
+
+export class StormGlass {
+    readonly stormGlassAPIParams =
+        'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+
+    readonly stormGlassAPISource = 'noaa';
+
+    constructor(protected request: AxiosStatic = axios) {}
+
+    public async fetchPoints(lat: number, lng: number): Promise<{}> {
+        return this.request.get(
+            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+        );
+    }
+}
