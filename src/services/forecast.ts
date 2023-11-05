d8341d10890468ff8268327d9747d72a4f297c61
augustoaccorsi <augusto.acorsi@gmail.com>
2023-11-05 16:56:58 -0300

    uodate forecasr service to mapo forecast by time

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -4,7 +4,7 @@ export enum BeachPosition {
     S = 'S',
     N = 'N',
     W = 'W',
-    E = 'E'
+    E = 'E',
 }
 
 export interface Beach {
@@ -15,17 +15,20 @@ export interface Beach {
     user: string;
 }
 
-export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export interface TimeForecast {
+    time: string,
+    forecast: BeachForecast[]
 }
 
 export class Forecast {
-
     constructor(protected stormGlass = new StormGlass()) {}
 
-    public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+    public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]> {
         const pointsWithCorrectSource: BeachForecast[] = [];
 
-        beaches.forEach(async (beach) => {
+        for (const beach of beaches) {
             const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
             const enrichedBeachData = points.map((point) => ({
                 ...{
@@ -33,15 +36,26 @@ export class Forecast {
                     lng: beach.lng,
                     name: beach.name,
                     position: beach.position,
-                    rating: 1
+                    rating: 1,
                 },
-                ...point
+                ...point,
             }));
-
             pointsWithCorrectSource.push(...enrichedBeachData);
-        });
+        }
 
-        return pointsWithCorrectSource;
+        return this.mapForecastByTime(pointsWithCorrectSource);
+    }
+
+    private mapForecastByTime(forecasts: BeachForecast[]): TimeForecast[] {
+        const forecastByTime: TimeForecast[] = [];
+
+        for (const forecast of forecasts) {
+            const timePoint = forecastByTime.find(f => f.time === forecast.time);
+            timePoint
+                ? timePoint.forecast.push(forecast)
+                : forecastByTime.push({ time: forecast.time, forecast: [forecast] });
+        }
+
+        return forecastByTime;
     }
 }
-
