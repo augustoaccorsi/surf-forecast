a5e257c426182d06069056ea44d94b3ce0ea2f63
augustoaccorsi <augusto.acorsi@gmail.com>
2023-11-05 21:52:55 -0300

    update index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { SetupServer } from './server';
+import config from 'config';
+
+//self called function
+(async(): Promise<void> => {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+})();
