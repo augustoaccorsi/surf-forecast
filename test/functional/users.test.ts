d5452c8bf42b4ea518fa42583ea984acffe6fadc
augustoaccorsi <augusto.acorsi@gmail.com>
2023-11-05 23:44:29 -0300

    add function to encrypt password

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,9 +1,10 @@
 import { User } from '@src/models/user';
+import AuthService from '@src/services/authservices';
 
 describe('User functional tests', () => {
     beforeAll(async () => await User.deleteMany({}));
     describe('when creating a new user', () => {
-        it('should successfuly create a new user', async () => {
+        it('should successfuly create a new user with encrypted password', async () => {
             const newUser = {
                 name: 'Mock User',
                 email: 'mock@mail.com',
@@ -12,7 +13,15 @@ describe('User functional tests', () => {
 
             const response = await global.testRequest.post('/users').send(newUser);
             expect(response.status).toBe(201);
-            expect(response.body).toEqual(expect.objectContaining(newUser));
+            await expect(
+                AuthService.comparePasswords(newUser.password, response.body.password)
+            ).resolves.toBeTruthy();
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    ...newUser,
+                    ...{ password: expect.any(String) },
+                })
+            );
         });
 
         it('should return 422 when there is a validation error', async () => {
